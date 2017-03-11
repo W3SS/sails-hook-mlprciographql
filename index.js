95dd6f1eec68ce2ee25c15e9c2924e0b79423f74
Daniel González Espinoza <danielglezespinoza@gmail.com>
2017-03-10 18:53:09 -0600

    Added original code

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ module.exports = function (sails) {
   return {
     initialize: function (next) {
       sails.after(["hook:orm:loaded", "hook:policies:loaded"], () => {
+        sails.graphql = (req) =>{
+          return graphQL.graphql(
+              sailsGraphQL.generateSchema(sails.models),
+              req.body.query,
+              null,
+              {
+                  request: sails.request,
+                  reqData: {
+                      headers: {}
+                  }
+              }
+          );
+        };
         return next();
       });
     }
